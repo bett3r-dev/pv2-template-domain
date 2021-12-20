3ef3a5cb80c6f61920326912f8c3cc60d97b35ef
Tomas Ruiz <tomasruizr@gmail.com>
2021-12-20 17:14:10 -0300

    [update] Cart Commands QuantityUpdated events

--- src/cart/cart.commands.ts
+++ src/cart/cart.commands.ts
@@ -1,5 +1,6 @@
 import { Command } from "@bett3r-dev/server-core";
 import joi from 'joi';
+import { CartEvents } from "src/domain/build/cart";
 import { CartProduct, CartProductSchema, CartErrors } from ".";
 
 export const CreateUserCart: Command<null> = () => ({
@@ -13,11 +14,14 @@ export const AddProduct: Command<CartProduct> = () => ({
 })
 
 export const UpdateQuantity: Command<{productId:string, quantity:number}> = () => ({
+  isPublic: true,
   schema: joi.object().keys({
     productId: joi.string().uuid(),
     quantity: joi.number()
   }),
-  isPublic: true,
+  events:[
+    CartEvents.QuantityUpdated
+  ],
   errors: [
     CartErrors.ProductOutOfStock,
     CartErrors.NegativeQuantity,
