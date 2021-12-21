21c94d79ae78dde1e28e5d0d03140f25a73ac4d3
Tomas Ruiz <tomasruizr@gmail.com>
2021-12-21 12:32:02 -0300

    [update] user model
    Co-authored-by: lcsalt <lcsalt@users.noreply.github.com>

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,9 +1,17 @@
 import joi from "joi";
 
 export type UserModel = {
+  id: string,
+  name: string,
+  lastName: string,
+  mail: string,
   address: string
 }
 
 export const UserModelSchema = joi.object().keys({
+  id: joi.string().uuid(),
+  name: joi.string(),
+  lastName: joi.string(),
+  mail: joi.string().email(),
   address: joi.string()
 })
