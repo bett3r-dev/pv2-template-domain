ed4f7313f2fb72df85b2c93c0991f773c7f53f95
Tomas Ruiz <tomasruizr@gmail.com>
2022-06-04 15:08:05 -0300

    [add] UserDoesNotExists Error

--- src/user/user.errors.ts
+++ src/user/user.errors.ts
@@ -1,2 +1,7 @@
 import joi from 'joi';
 import { AppError } from '@bett3r-dev/server-core';
+
+export const UserDoesNotExist: AppError<string[]> = () => ({
+  schema: joi.array().items(joi.string()),
+  type: 'NotFoundError',
+})
